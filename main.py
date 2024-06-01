a1b894ad71d721a81876078fcd4d7ecd5caaa26b
Ravin Gupta <ravingupta129@gmail.com>
2024-05-31 23:00:05 -0700

    kafka connection setup

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from fastapi import Depends, FastAPI
 
 from .config.redis import get_redis
+from .config.kafka import KafkaConnector, KafkaConsumer
 
 from .routes import users, urls
 
@@ -16,6 +17,24 @@ app.include_router(urls.router)
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/kafka-test")
+def kafka_connection_test():
+    kfc = KafkaConnector()
+    kc = KafkaConsumer()
+    kfc.produce('test', "This is a sample message")
+
+    # consumer = kfc.__get_consumer('123', 'test')
+
+    print("Trial 1")
+    kfc.consume('123', 'test')
+
+    # print("Trial 2")
+    # kc.consume(consumer)
+
+    # producer = kfc.__get_producer()
+    
+    return {"message": "Done"}
 
 @app.get("/redis-test")
 def redis_connection_test(cache = Depends(get_redis)):
